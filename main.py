79423a596d940e06276a5ac7af9b8b644c48ce76
manavnagpal08 <72203264+manavnagpal08@users.noreply.github.com>
2025-12-15 09:13:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# login.py
-import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(layout="centered")
-
-# 🔐 Listen for auth result
-auth_data = st.experimental_get_query_params()
-
-st.markdown("## 🔐 ScreenerPro Login")
-
 components.html(
-    open("login.html", "r", encoding="utf-8").read(),
-    height=720,
-    scrolling=False,
+    open("login.html", encoding="utf-8").read(),
+    height=900,
+    scrolling=False
 )
